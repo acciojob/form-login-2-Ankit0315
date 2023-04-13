ed47ed7f91ba115481d0d939b2b03eb61ad5bb4f
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-13 17:58:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function myfunction(){
-	var fname=document.getElementById("firstname").value;
-	var lname=document.getElementById("lastname").value;
-	var pno=document.getElementById("phone").value;
-	var email=document.getElementById("mail").value;
-	cost btn=document.getElementById("btn");
-	btn.style.backgroundColor='blue';
-	alert(`First Name: ${fname}\nLast Name: ${lname}\nPhone Number: ${pno}\nEmail ID: ${email}`);
+function AlertFunction(){
+	var x1 = document.getElementById('text1').value;
+	var x2 =  document.getElementById('text2').value;
+	var x3 =  document.getElementById('num').value;
+	var x4 =  document.getElementById('mail').value;
+	
+	alert("First Name: "+x1+"\n Last Name: "+x2+"\n Phone Number: "+x3+"\n Email ID: "+x4);
 }
