de2edae1f73146c2b0e0359124046cdd99ce6734
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-13 17:42:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+function submit() {
+const fname= document.getElementById("fname").value;
+const lname=document.getElementById("lname").value;
+const phone=document.getElementById("number").value;
+const email=document.getElementById("email").value;
+
+	alert(fname+lname+phone+email);
+	
+}
